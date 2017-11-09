e25ae14b5e9353a1ac01ac6d932c92960e11d08b
Abdullah Khaled Bassim Aly Mohamed <abdullah.aly@student.guc.edu.eg>
2017-11-09 17:42:29 +0200

    feat: Add foreign keys to Attendances table

--- DB.sql
+++ DB.sql
@@ -26,13 +26,14 @@ salary INT NOT NULL,
 company_email VARCHAR(70) NOT NULL,
 department_code INT NOT NULL,
 company_domain VARCHAR(50) NOT NULL,
-FOREIGN KEY (user_name) REFERENCES Users(user_name) ON DELETE SET NULL ON UPDATE CASCADE,
-FOREIGN KEY (department_code,company_domain) REFERENCES Departments (department_code,company_domain) ON DELETE SET NULL ON UPDATE CASCADE
+FOREIGN KEY (user_name) REFERENCES Users(user_name) ON DELETE CASCADE ON UPDATE CASCADE,
+FOREIGN KEY (department_code,company_domain) REFERENCES Departments (department_code,company_domain) ON DELETE CASCADE ON UPDATE CASCADE
 )
 CREATE TABLE Attendances(
 PRIMARY KEY (user_name,date),
 user_name VARCHAR(30) NOT NULL,
 date DATETIME NOT NULL,
 start_time DATETIME NOT NULL,
-leave_time DATETIME NOT NULL
+leave_time DATETIME NOT NULL,
+FOREIGN KEY (user_name) REFERENCES Staff_Members(user_name) ON DELETE CASCADE ON UPDATE CASCADE
 )
