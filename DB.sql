7202d7b655986976cc2bbb3cce3d5f32f47293a0
Abdullah Khaled Bassim Aly Mohamed <abdullah.aly@student.guc.edu.eg>
2017-11-09 16:34:58 +0200

    feat: Add foreign keys to Job_Seekers table

--- DB.sql
+++ DB.sql
@@ -10,5 +10,7 @@ last_name VARCHAR(25),
 age  AS (YEAR(CURRENT_TIMESTAMP)-YEAR(birth_date))
 )
 create table Job_Seekers(
-user_name VARCHAR(50) PRIMARY KEY
+user_name VARCHAR(50) PRIMARY KEY,
+FOREIGN KEY (user_name) REFERENCES Users(user_name) ON DELETE SET NULL ON UPDATE CASCADE
+
 );
