345ded4782ca005d1490722f7c2eac9071278787
Logician724 <king2sairos2013@gmail.com>
2017-11-09 16:16:12 +0200

    feat: Add Users table

--- DB.sql
+++ DB.sql
@@ -1,1 +1,11 @@
-//put your creations in here
+--put your creations in here
+CREATE TABLE Users (
+user_name VARCHAR(15) PRIMARY KEY,
+password VARCHAR(30),
+personal_email VARCHAR(50),
+birth_date DATETIME,
+exp_year INT,
+first_name VARCHAR(25),
+last_name VARCHAR(25),
+age  AS (YEAR(CURRENT_TIMESTAMP)-YEAR(birth_date))
+)
