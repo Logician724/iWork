d1c89d736da48d41e06d09858e1d6b5415be1c93
Abdullah Khaled Bassim Aly Mohamed <abdullah.aly@student.guc.edu.eg>
2017-11-09 17:37:02 +0200

    feat: Add Attendances table

--- DB.sql
+++ DB.sql
@@ -10,29 +10,29 @@ last_name VARCHAR(25) NOT NULL,
 age  AS (YEAR(CURRENT_TIMESTAMP)-YEAR(birth_date)) NOT NULL
 )
 CREATE TABLE USER_Prev_Jobs(
-user_name VARCHAR(30) PRIMARY KEY,
-prev_job VARCHAR(15) PRIMARY KEY,
+user_name VARCHAR(30) PRIMARY KEY NOT NULL,
+prev_job VARCHAR(15) PRIMARY KEY NOT NULL,
 FOREIGN KEY (user_name) REFERENCES Users(user_name) ON DELETE CASCADE ON UPDATE CASCADE
 )
 CREATE TABLE Job_Seekers(
-user_name VARCHAR(30) PRIMARY KEY,
+user_name VARCHAR(30) PRIMARY KEY NOT NULL,
 FOREIGN KEY (user_name) REFERENCES Users(user_name) ON DELETE CASCADE ON UPDATE CASCADE
 )
 CREATE TABLE Staff_Members( 
-user_name VARCHAR(30) PRIMARY KEY,
-day_off VARCHAR(15),
-no_annual_leaves INT,
-salary INT,
-company_email VARCHAR(70),
-department_code INT,
-company_domain VARCHAR(50),
+user_name VARCHAR(30) PRIMARY KEY NOT NULL,
+day_off VARCHAR(15) NOT NULL,
+no_annual_leaves INT NOT NULL,
+salary INT NOT NULL,
+company_email VARCHAR(70) NOT NULL,
+department_code INT NOT NULL,
+company_domain VARCHAR(50) NOT NULL,
 FOREIGN KEY (user_name) REFERENCES Users(user_name) ON DELETE SET NULL ON UPDATE CASCADE,
 FOREIGN KEY (department_code,company_domain) REFERENCES Departments (department_code,company_domain) ON DELETE SET NULL ON UPDATE CASCADE
 )
 CREATE TABLE Attendances(
-user_name VARCHAR(30) ,
-date DATETIME,
-start_time DATETIME ,
-leave_time DATETIME,
-PRIMARY KEY (user_name,date)
+PRIMARY KEY (user_name,date),
+user_name VARCHAR(30) NOT NULL,
+date DATETIME NOT NULL,
+start_time DATETIME NOT NULL,
+leave_time DATETIME NOT NULL
 )
