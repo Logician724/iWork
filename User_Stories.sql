0d60a2516300f27801ecf2e5ac1d20735975f568
Logician724 <king2sairos2013@gmail.com>
2017-11-14 12:15:24 +0200

    feat: Add UserLoginSP procedure

--- User_Stories.sql
+++ User_Stories.sql
@@ -6,4 +6,53 @@ CREATE PROC ViewDepartmentSP
 AS
 SELECT d.*
 FROM Departments d
-WHERE ((d.company_omain = @companyDomain) AND (d.department_code = @departmentCode))
+WHERE ((d.company_domain = @companyDomain) AND (d.department_code = @departmentCode))
+
+GO
+
+CREATE PROC UserLoginSP
+@userName VARCHAR(30),
+@password  VARCHAR(30)
+AS
+BEGIN
+IF
+NOT EXISTS (
+SELECT u.*
+FROM Users u
+WHERE (u.user_name = @userName AND u.password = @password)
+)
+
+PRINT 'User not found'
+
+ELSE IF
+EXISTS ( 
+SELECT s.*
+FROM Job_Seekers s
+WHERE (s.user_name = @userName)
+)
+PRINT 'Hello Seeker'
+
+ELSE IF
+EXISTS (
+SELECT m.*
+FROM Managers m
+WHERE (m.user_name = @userName)
+)
+PRINT 'Hello Manager'
+ELSE IF
+EXISTS (
+SELECT h.*
+FROM HR_Employees h
+WHERE (h.user_name = @userName)
+)
+PRINT  'Hello HR'
+
+ELSE IF
+EXISTS (
+SELECT r.*
+FROM Regular_Employees r
+WHERE (r.user_name = @userName)
+)
+PRINT 'Hello Employee'
+
+END
