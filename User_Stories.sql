d3b6c8cf07df4da1f456d2b3675fb75c2a5566b4
Logician724 <king2sairos2013@gmail.com>
2017-11-14 10:34:28 +0200

    feat: Add ViewDepartmentSP procedure

--- User_Stories.sql
+++ User_Stories.sql
@@ -1,1 +1,9 @@
-﻿
+﻿GO
+
+CREATE PROC ViewDepartmentSP
+@companyDomain VARCHAR(150),
+@departmentCode VARCHAR(30)
+AS
+SELECT d.*
+FROM Departments d
+WHERE ((d.company_omain = @companyDomain) AND (d.department_code = @departmentCode))
