e90243dd61de817544dec0ed8867d144fa7b8ecd
Yasmine Mustafa <yasminemustafah@gmail.com>
2017-11-15 16:13:01 +0200

    feat: Add insertions for users

--- Insertions.sql
+++ Insertions.sql
@@ -15,6 +15,23 @@ VALUES('amazon.com','amazon','Beacon Hill neighborhood','computer marketing',
 INSERT INTO Users VALUES ('Yousef.Mustafa', 'WWE123','yousef.mustafa@gmail.com','12/11/1993',2,'Yousef', 'Mustafa'); 
 INSERT INTO Users VALUES ('Fayrouz.Hussain','02306FM','fayrouz.hussain@hotmail.com','06/23/1994',1,'Fayrouz','Hussain');
 INSERT INTO Users VALUES ('Dina.AlMasry','DMAm111','dinamohamed@gmail.com','11/1/1990',5,'Dina', 'AlMasry');
+INSERT INTO Users VALUES ('mirna.alteer','mirna12321','mirna.alteer@yahoo.com','2/2/1992',1,'Mirna','Alteer');
+INSERT INTO Users VALUES ('sally.ramadan','miss341','sally@yahoo.com','2/5/1993',3,'Sally','Mohamed');
+INSERT INTO Users VALUES ('firdous.ahmed','afH12321','fifo12@gmail.com','12/9/1985',6,'Firdous','Ahmed');
+INSERT INTO Users VALUES ('hadeel.mohamed','Hadeel12321','hadeel@outlook.com','3/3/1992',0,'Hadeel','Moahmed');
+INSERT INTO Users VALUES ('islam.osama','osama00hs','islamosama@hotmail.com','10/10/1985',10,'Islam','Osama');
+INSERT INTO Users VALUES ('mohamed.osama','ma1s8hds','mohamed.osama@gmail.com','10/2/1983',12,'Mohamed','Osama');
+INSERT INTO Users VALUES ('dana.debs','ddns34','dana.debs1995@yahoo.com','2/9/1995',3,'Dana','Debs');
+INSERT INTO Users VALUES ('ahmed.hussain','asdfg567','ahmedH@live.com','9/10/1990',3,'Ahmed','AlTayeb');
+INSERT INTO Users VALUES ('amr.mohsen','rthdfv3435','amrMohsen@yahoo.com','7/20/1989',3,'Amr','Ahmed');
+INSERT INTO Users VALUES ('dahlia.karass','dahkar238','karass.dahlia@outlook.com','12/10/1996',0,'Dahlia','Karas');
+INSERT INTO Users VALUES ('ahmed.shehata','lps8034','ahmed.s@gmail.com','2/2/1984',6,'Ahmed','Morsey');
+INSERT INTO Users VALUES ('youmna.marzook','seegsd0097','youmna.marzook@yahoo.com','5/27/1994',2,'Youmna','Marzook');
+INSERT INTO Users VALUES ('bonita.bon','msibee90','bonita@yahoo.com','6/21/1960',15,'Bonita','Bon');
+INSERT INTO Users VALUES ('basma.mohamed','soweds342','basma.mohamed@hotmail.com','3/2/1995',1,'Mirna','Alteer');
+INSERT INTO Users VALUES ('salma.tarek','tarek12321','salma.tarek@gmail.com','8/9/1990',2,'Salma','Hussain');
+
+
 
 INSERT INTO Job_Seekers VALUES ('Yousef.Mustafa');
 INSERT INTO Job_Seekers VALUES ('Fayrouz.Hussain');
