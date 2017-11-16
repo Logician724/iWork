1b88b552c8cac0a666d2a4427b8bc04fb2a2d2e6
gharam37 <g_z48@yahoo.com>
2017-11-16 09:25:57 -0800

    feat : Test and Execute RegisterToWebsite Procedure

---
+++ RomyExec.sql
@@ -0,0 +1,19 @@
+﻿-- Romy Started  fixing here
+--DROP PROC RegisterToWebsite;
+GO
+CREATE PROC RegisterToWebsite   -- Handles 5 in unregistered user
+@userName VARCHAR(30)  , --takes entity as input 
+@password VARCHAR(30) ,
+@personalEmail VARCHAR(70) ,
+@birthDate DATETIME ,
+@expYear INT ,
+@firstName VARCHAR(25) ,
+@lastName VARCHAR(25) 
+AS
+insert into Users
+Values(@username,@password,@personalEmail,@birthDate,@expYear,@firstName,@lastName)
+
+
+
+-- She eneded here 
+EXEC RegisterToWebsite 'Mohamed_Zaki','123455','Zaki@guc.edu.eg','05/05/2012',10,'Mohamed','Zaki' --executes the procedure 
