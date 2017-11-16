9815438d764834e4541de65b12b4a758ae53e6a1
abdo16khaled <abdullah16khaled@gmail.com>
2017-11-16 20:20:43 +0200

    Feat: Add Execution to execution file

--- Executions.sql
+++ Executions.sql
@@ -3,3 +3,5 @@
 EXEC  UserLoginSP 'Yousef.Mustafa','1234'
 
 EXEC ViewCompanySP 'facebook',' Palo Alto','computer marketing'
+
+EXEC ViewQuetionsInInterviewSP  'Manager- Managing PHP Department','PHP-5019','facebook.com'
