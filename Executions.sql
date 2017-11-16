2b8721cc90429dadd0fd3cfcec4864b664e95197
abdo16khaled <abdullah16khaled@gmail.com>
2017-11-16 22:02:26 +0200

    Feat: Add executions to execution file

--- Executions.sql
+++ Executions.sql
@@ -5,3 +5,14 @@ EXEC  UserLoginSP 'Yousef.Mustafa','1234'
 EXEC ViewCompanySP 'facebook',' Palo Alto','computer marketing'
 
 EXEC ViewQuetionsInInterviewSP  'Manager- Managing PHP Department','PHP-5019','facebook.com'
+
+EXEC DeletePendingApplicationSP 'shadi.aly','Manager- Managing PHP Department','PHP-5019','facebook.com'
+
+EXEC ViewAttendanceSP 'salma.tarek','2017-8-20','2017-8-22'
+
+EXEC SendEmailSP 'andra.kyla','andra.kyla@facebook.com','Yousef.Mustafa','Yousef.Mustafa@facebook.com','Plan Delivery','To Mr. Yousef, I would like to inform you that the plan we agreed on has been revised and ready for delivery. When would you like us to deliver it? From, Andra'
+
+EXEC AddJobSP 'Dina.AlMasry','Employee- Debuging Handling PHP Department','PHP-5019','facebook.com','2018-2-28',
+				'Employees will handle testing codes produced by other coders in the department and debuging those codes',4,10000,
+				'Handling testing and debuging code in PHP Department',8,7
+
