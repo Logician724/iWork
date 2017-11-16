fdae7c456d623b765d18321f90fd855e715d8045
gharam37 <g_z48@yahoo.com>
2017-11-16 13:45:19 -0800

    feat : Add ChooseJobFromAcceptedAppSP execution partially correct check comment

--- Executions.sql
+++ Executions.sql
@@ -10,3 +10,11 @@ EXEC ViewDepartmentsOfCompanySP 'facebook.com' --View departments in a certian c
 
 EXEC ViewCompaniesSalariesSP; --View companies ordered by their average salary
 EXEC ApplyForJobSP 'gawwad.gooda','Employee- BootStrap Developer','BS-3291','facebook.com'
+
+INSERT INTO Applications (seeker_username,job_title,department_code,company_domain,app_status) Values ('shadi.aly','Employee- BootStrap Developer','BS-3291','facebook.com','Accepted') 
+
+EXEC ChooseJobFromAcceptedAppSP 
+'shadi.aly','BS-3291','facebook.com','Employee- BootStrap Developer','Sunday' --Creates a referencing conflict 
+
+EXEC ChooseJobFromAcceptedAppSP 
+'shadi.aly','BS-3291','facebook.com','Employee- BootStrap Developer','Friday ' --Works correctly returns error
