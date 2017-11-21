5589ad7221fbafb29dd1614fd93ae92d31ae0789
Yasmine Mustafa <yasminemustafah@gmail.com>
2017-11-21 15:49:52 +0200

    fix: Add a missing condition in DefineTaskSP

--- User_Stories.sql
+++ User_Stories.sql
@@ -1983,6 +1983,13 @@ AND @projectName = project_name
 SET @operationStatus = 1
 END
 --8: Yasmine------------------------------------------------------------------------------------------------------------------------------------
+
+--Manager User Stories no.8:-
+--Managers can define a task in a project in their departments which will have status ‘Open’.
+--The procedure takes as inputs the manager Username, the project name, deadline of the task, task name, comments for the task, and description for the task
+-- 
+
+
 GO 
 CREATE PROC DefineTaskSP
 @managerUsername VARCHAR(30),
@@ -1998,7 +2005,7 @@ SELECT *
 FROM Staff_Members s1 INNER JOIN Projects p 
 ON s1.user_name = p.manager_user_name
 INNER JOIN Staff_Members s2
-ON s2.department_code = s1.department_code
+ON s2.department_code = s1.department_code AND S2.company_domain=S1.company_domain
 WHERE @managerUsername = p.manager_user_name
 )
 SET @operationStatus = 0
