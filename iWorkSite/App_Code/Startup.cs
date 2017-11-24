10f32cddacc0ad020796ab6a4a8e326ddb023f4e
Logician724 <king2sairos2013@gmail.com>
2017-11-24 08:31:50 +0200

    chore: Initialize iWorkSite

---
+++ iWorkSite/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(iWorkSite.Startup))]
+namespace iWorkSite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
