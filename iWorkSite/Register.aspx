9910e0f0d46051371b820a33b7780ed4fbceff06
Yasmine Mustafa <yasminemustafah@gmail.com>
2017-11-29 20:55:18 +0200

    feat: Add method and html for Register

---
+++ iWorkSite/Register.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Register.aspx.cs" Inherits="Register" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+      <div class="main-container center-container">   
+        <form class="center-container" runat="server">
+            <div class="center-container">
+                <label class ="label">Username</label>
+                <asp:TextBox CssClass="txtbox input-lg" ID="txt_username" runat="server" placeholder="Enter your username" ></asp:TextBox>
+                <label class="label">Password</label>
+                <asp:TextBox CssClass="txtbox input-lg" ID="txt_password" runat="server" TextMode="Password" placeholder="Enter your password" ></asp:TextBox>
+                <label class="label">Email</label>
+                <asp:TextBox CssClass="txtbox input-lg" ID="txt_email" runat="server" placeholder="Eg: example@example.com" ></asp:TextBox>
+                <label class="label">Birthdate</label>
+                <asp:TextBox CssClass="txtbox input-lg" ID="txt_birthdate" runat="server" placeholder="M/D/Y" ></asp:TextBox>
+                <label class="label">Number of Experience Years</label>
+                <asp:TextBox CssClass="txtbox input-lg" ID="txt_expyrs" runat="server" placeholder="Enter number" ></asp:TextBox>
+                <label class="label">First Name</label>
+                <asp:TextBox CssClass="txtbox input-lg" ID="txt_fn" runat="server" placeholder="Enter First Name" ></asp:TextBox>
+                <label class="label">Last Name</label>
+                <asp:TextBox CssClass="txtbox input-lg" ID="txt_ln" runat="server" placeholder="Enter Last Name" ></asp:TextBox>
+                
+                <asp:Button CssClass="button btn btn-success" ID="btn_login" runat="server" Text="Register" onclick="register"/>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
---
+++ iWorkSite/Register.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+public partial class Register : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+                  
+    }
+
+    protected void register(object sender, EventArgs e)
+    {
+
+        string connStr = ConfigurationManager.ConnectionStrings["iWorkDbConn"].ToString();
+        SqlConnection conn = new SqlConnection(connStr);
+        SqlCommand cmd = new SqlCommand("RegisterToWebsite", conn);
+        cmd.CommandType = CommandType.StoredProcedure;
+        string username = txt_username.Text;
+        string password = txt_password.Text;
+        string personalemail = txt_email.Text;
+        string birthdate = txt_birthdate.Text;
+        int expyear = Convert.ToInt32(txt_expyrs.Text);
+        string firstname = txt_fn.Text;
+        string lastname = txt_ln.Text;
+        cmd.Parameters.Add(new SqlParameter("@userName", username));
+        cmd.Parameters.Add(new SqlParameter("@password", password));
+        cmd.Parameters.Add(new SqlParameter("@personalEmail", personalemail));
+        cmd.Parameters.Add(new SqlParameter("@birthDate", birthdate));
+        cmd.Parameters.Add(new SqlParameter("@expYear", expyear));
+        cmd.Parameters.Add(new SqlParameter("@firstname", firstname));
+        cmd.Parameters.Add(new SqlParameter("@lastname", lastname));
+        //output parameterssub
+        SqlParameter OperationStatus = cmd.Parameters.Add("@operationStatus", SqlDbType.Bit);
+        OperationStatus.Direction = ParameterDirection.Output;
+        conn.Open();
+        cmd.ExecuteNonQuery();
+        conn.Close();
+        Response.Write(OperationStatus.Value.ToString());
+        switch (OperationStatus.Value.ToString())
+        {
+            case "0":
+                Response.Write("This username is already in use");
+                break;
+            case "1":
+                Session["Username"] = username;
+                Response.Write("Successfully Registered!");
+                Response.Redirect("Companies", true);
+                break;
+        }
+
+    }
+}
