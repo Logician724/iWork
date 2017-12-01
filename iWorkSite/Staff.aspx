a4f8402676b88e14614a3fd8993a89ba8b7e5b98
Yasmine Mustafa <yasminemustafah@gmail.com>
2017-12-01 13:50:38 +0200

    feat: Add checkin and checkout methods

--- iWorkSite/Staff.aspx
+++ iWorkSite/Staff.aspx
@@ -5,11 +5,17 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+      <link rel="stylesheet" type="text/css" href="Content/bootstrap.css"/>
 </head>
 <body>
     <form id="form1" runat="server">
-        <div>
-        </div>
+
+        <asp:Button CssClass="btn btn-primary" ID="Button2" runat="server" Text="Check In" OnClick="CheckIn" />
+        <div class="text-center mt-2 font-weight-bold text-danger" id="checkin_response" runat="server"></div>
+        
+        <asp:Button CssClass="btn btn-primary" ID="Button1" runat="server" Text="Check Out" OnClick="CheckOut" />
+        <div class="text-center mt-2 font-weight-bold text-danger" id="checkout_response" runat="server"></div>
+
     </form>
 </body>
 </html>
--- iWorkSite/Staff.aspx.cs
+++ iWorkSite/Staff.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
 
 public partial class Staff : System.Web.UI.Page
 {
@@ -11,4 +15,82 @@ public partial class Staff : System.Web.UI.Page
     {
 
     }
+
+
+    protected void CheckIn(object sender, EventArgs e)
+    {
+        string Username = Session["Username"].ToString();
+
+        string connStr = ConfigurationManager.ConnectionStrings["iWorkDbConn"].ToString();
+        SqlConnection conn = new SqlConnection(connStr);
+        SqlCommand cmd = new SqlCommand("CheckInSP", conn);
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.Parameters.Add(new SqlParameter("@userName", Username));
+        //output parameters
+        SqlParameter OperationStatus = cmd.Parameters.Add("@operationStatus", SqlDbType.Bit);
+        OperationStatus.Direction = ParameterDirection.Output;
+        conn.Open();
+        cmd.ExecuteNonQuery();
+        string op = OperationStatus.Value.ToString();
+        conn.Close();
+   
+        Label failed = new Label();
+        failed.Text = "It's your day-off !";
+        Label passed = new Label();
+        passed.Text = "Successfully Checked In.";
+
+        switch (op)
+        {
+            case "False":
+                checkin_response.Controls.Add(failed);
+                break;
+            case "True":
+                checkin_response.Controls.Add(passed);
+                break;
+ 
+        }
+
+
+    }
+
+    //---------------------------------------------------------------------------------------------------
+
+
+    protected void CheckOut(object sender, EventArgs e)
+    {
+        string Username = Session["Username"].ToString();
+
+        string connStr = ConfigurationManager.ConnectionStrings["iWorkDbConn"].ToString();
+        SqlConnection conn = new SqlConnection(connStr);
+        SqlCommand cmd = new SqlCommand("CheckOutSP", conn);
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.Parameters.Add(new SqlParameter("@userName", Username));
+        //output parameters
+        SqlParameter OperationStatus = cmd.Parameters.Add("@operationStatus", SqlDbType.Bit);
+        OperationStatus.Direction = ParameterDirection.Output;
+        conn.Open();
+        cmd.ExecuteNonQuery();
+        string op = OperationStatus.Value.ToString();
+        conn.Close();
+
+        Label failed = new Label();
+        failed.Text = "It's your day-off !";
+        Label passed = new Label();
+        passed.Text = "Successfully Checked Out.";
+
+        switch (op)
+        {
+            case "False":
+                checkout_response.Controls.Add(failed);
+                break;
+            case "True":
+                checkout_response.Controls.Add(passed);
+                break;
+
+        }
+
+
+    }
+
+
 }
